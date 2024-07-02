8bf85c9c638d43878ecd70818ac06241be845308
Kfiros <nevo.kfir@gmail.com>
2024-07-02 12:09:15 +0300

    initial commit

---
+++ src/test/java/frc/robot/ExampleTest.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleTest
+{
+    @Test
+    void twoPlusTwoShouldEqualFour()
+    {
+        assertEquals(4, 2 + 2);
+    }
+}
